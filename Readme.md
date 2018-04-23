e61cc318dd3596fb160b6784d28d6188985fbb3b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to print a report
+
+
+<p>The following example demonstrates how to print a report.</p><p>See also: <br />
+- <a href="https://www.devexpress.com/Support/Center/p/E1762">How to print a report without ReportToolbar</a>;<br />
+- <a href="https://www.devexpress.com/Support/Center/p/E1766">How to programmatically select a printer</a>;<br />
+- <a href="https://www.devexpress.com/Support/Center/p/E332">How to dynamically select the paper source and set the printer resolution</a>.</p>
+
+<br/>
+
+
---
+++ VB/PrintReport/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraReports.UI
+
+Namespace PrintReport
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			' Create a report instance, assigned to a Print Tool.
+			Dim pt As New ReportPrintTool(New XtraReport1())
+
+			' Invoke the Print dialog.
+			pt.PrintDialog()
+
+			' Send the report to the default printer.
+			pt.Print()
+
+			' Send the report to the specified printer.
+			pt.Print("myPrinter")
+		End Sub
+
+	End Class
+End Namespace
